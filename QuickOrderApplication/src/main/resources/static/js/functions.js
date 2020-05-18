914f6ab67807a3f5e1db84998eab1c0fce609050
Yauheniya Nikulyak <ynikulyak@csumb.edu>
2020-05-18 12:43:58 -0700

    added buttons eventListeners to the quantity

--- QuickOrderApplication/src/main/resources/static/js/functions.js
+++ QuickOrderApplication/src/main/resources/static/js/functions.js
@@ -1,2 +1,16 @@
 console.log("Now: " + new Date());
+window.onload = function() {
+	var input = document.getElementById("item-qty");
 
+	document.getElementById("plus").onclick = function() {
+		input.value = parseInt(input.value, 10) + 1
+	}
+
+	document.getElementById("minus").onclick = function() {
+		if (input.value == 1) {
+			return;
+		} else {
+			input.value = parseInt(input.value, 10) - 1
+		}
+	}
+}
